55f80a7277357318b9c5e4b7bbfa951909bbad28
Mahir Eldaw <meldaw1@nsmb208-xx.csudh.edu>
2018-03-19 16:46:56 -0700

    Version 1

---
+++ room12.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+void main(void)
+{
+	 int choice=0;
+	 puts("Welcome this is room 12");
+	 puts("What would you like to do: \n1. Leave\n2.Stay");
+	 scanf("%d",&choice);
+	 if(choice==1)
+	 {
+	 puts("Thank You Come Again");	
+	 }
+	 
+	 if(choice==2)
+	 {
+	 	puts("Let's play some games");
+	 }
+}
