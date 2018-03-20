2de9fef1ca3ee1248a76273e6f54c39ed627ab9d
Mahir Eldaw <meldaw1@nsmb208-xx.csudh.edu>
2018-03-19 17:10:55 -0700

    Version 3

--- room12.c
+++ room12.c
@@ -1,14 +1,21 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <ctype.h>
+#include <time.h>
+
+#define SIDES 6
+#define MagicNumber 10
 
 void main(void)
 {
 	 int choice=0;
 	 int input=0;
+	 srand(time(NULL));
+
 	 puts("Welcome this is room 12");
 	 puts("What would you like to do: \n1. Leave Room 12\n2.Stay");
 	 scanf("%d",&choice);
+
 	 if(choice==1)
 	 {
 	 puts("Thank You Come Again");	
@@ -26,7 +33,40 @@ void main(void)
 			if(input==1)
 			{
 				puts("Welcome to Guess the Number Game");
+				int a=0;
+				int b=(rand() % MagicNumber);;
+					
+
+				while(a!=b)
+				{
+				printf("Write a number between 0 and 9 exclusive\n");
+				scanf("%d",&a);
+
+				while(a<=0 && a>=11)
+				{
+				printf("Number is out of bounds\n");
+				printf("Write a number between 0 and 11 exclusive\n");
+				scanf("%d",&a);
+				}
+
+				if(a<b)
+				{
+				printf("Too low \n");
+				}
+
+				if(a>b)
+				{
+				printf("Too high \n");
+				}
+
+				if(a==b)
+				{
+				printf("Correct guess\n");
+				}
+
 			}
+				
+		}
 
 			else if(input==2)
 			{
