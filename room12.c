ee2f80666336c4d59d9b88cf7e021b70707e952e
Mahir Eldaw <meldaw1@nsmb208-xx.csudh.edu>
2018-03-19 17:03:00 -0700

    Version 2

--- room12.c
+++ room12.c
@@ -1,11 +1,13 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <ctype.h>
 
 void main(void)
 {
 	 int choice=0;
+	 int input=0;
 	 puts("Welcome this is room 12");
-	 puts("What would you like to do: \n1. Leave\n2.Stay");
+	 puts("What would you like to do: \n1. Leave Room 12\n2.Stay");
 	 scanf("%d",&choice);
 	 if(choice==1)
 	 {
@@ -14,6 +16,37 @@ void main(void)
 	 
 	 if(choice==2)
 	 {
-	 	puts("Let's play some games");
-	 }
+	 	while(input!=5)
+	 	{
+	 		puts("Let's play some games");
+			puts("Choose a game");
+			printf("Games:\n1. Guess the number\n2. Word Modification\n3. Guess the word\n4. Roll a dice\n5. Leave Room 12\n");
+			scanf("%d",&input);
+
+			if(input==1)
+			{
+				puts("Welcome to Guess the Number Game");
+			}
+
+			else if(input==2)
+			{
+				puts("Welcome to the word Modification Game");
+			}
+
+			else if(input==3)
+			{
+				puts("Welcome to Guess the word Game");
+			}
+
+			else if(input==4)
+			{
+				puts("Welcome to Roll the Dice Game");
+			}
+
+			else if(input==5)
+			{
+				puts("Thank You Come Again");
+			}
+		 }
+	}
 }
